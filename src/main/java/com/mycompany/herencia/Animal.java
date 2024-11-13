5887e5197dc74c64321055302bc79a9d6867a0ec
Carlosy06 <cychavez@uth.hn>
2024-11-12 18:48:43 -0600

    Creando y cambiando la interfaz.

---
+++ src/main/java/com/mycompany/herencia/Animal.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.herencia;
+
+/**
+ *
+ * @author carli
+ */
+public class Animal {
+    private String Pelo;
+    private String Boca;
+    private String Cola;
+    private String Ojo;
+    private Integer Pata;
+
+    public Animal(String Pelo, String Boca, String Cola, String Ojo, Integer Pata) {
+        this.Pelo = Pelo;
+        this.Boca = Boca;
+        this.Cola = Cola;
+        this.Ojo = Ojo;
+        this.Pata = Pata;
+    }
+
+    public String getPelo() {
+        return Pelo;
+    }
+
+    public void setPelo(String Pelo) {
+        this.Pelo = Pelo;
+    }
+
+    public String getBoca() {
+        return Boca;
+    }
+
+    public void setBoca(String Boca) {
+        this.Boca = Boca;
+    }
+
+    public String getCola() {
+        return Cola;
+    }
+
+    public void setCola(String Cola) {
+        this.Cola = Cola;
+    }
+
+    public String getOjo() {
+        return Ojo;
+    }
+
+    public void setOjo(String Ojo) {
+        this.Ojo = Ojo;
+    }
+
+    public Integer getPata() {
+        return Pata;
+    }
+
+    public void setPata(Integer Pata) {
+        this.Pata = Pata;
+    }
+    
+    
+    public void Sonido (){
+        
+    } 
+}
---
+++ src/main/java/com/mycompany/herencia/Herencia.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herencia;
+/**
+ *
+ * @author carli
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Herencia {
+
+    public static void main(String[] args) {
+        Animal p = new Perro ("Negro", "Si", "Si", "Si", 4);
+        p.Sonido();
+        
+        Animal g = new Gato ("Blanco", "Si", "Si", "Si", 4);
+        g.Sonido();
+        
+        List <Animal> Lista = new ArrayList <Animal>();
+        Lista.add (p);
+        Lista.add (g);
+        Lista.add(new Queqo ("No", "Si", "Si", "Si", 4));
+        
+        for (Animal item : Lista){
+            item.Sonido();
+        } 
+    }
+}
