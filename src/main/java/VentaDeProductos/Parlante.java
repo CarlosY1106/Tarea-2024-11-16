b3bb3fe4f6c9df1b662aac5a6e8df8a2187c06cc
Carlosy06 <cychavez@uth.hn>
2024-11-23 18:50:28 -0600

    Clases, herencias y formularios del objeto de Venta de productos.

---
+++ src/main/java/VentaDeProductos/Parlante.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VentaDeProductos;
+
+/**
+ *
+ * @author carli
+ */
+public class Parlante {
+    private String Sonido;
+    private String Rendimiento;
+
+    public Parlante(String Sonido, String Rendimiento) {
+        this.Sonido = Sonido;
+        this.Rendimiento = Rendimiento;
+    }
+
+    public String getSonido() {
+        return Sonido;
+    }
+
+    public void setSonido(String Sonido) {
+        this.Sonido = Sonido;
+    }
+
+    public String getRendimiento() {
+        return Rendimiento;
+    }
+
+    public void setRendimiento(String Rendimiento) {
+        this.Rendimiento = Rendimiento;
+    }
+    
+}
---
+++ src/main/java/VentaDeProductos/Refrigerador.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VentaDeProductos;
+
+/**
+ *
+ * @author Carlitos Chávez.
+ */
+public class Refrigerador {
+    private Integer Puertas;
+    private Integer Tamaño;
+
+    public Refrigerador(Integer Puertas, Integer Tamaño) {
+        this.Puertas = Puertas;
+        this.Tamaño = Tamaño;
+    }
+
+    public Integer getPuertas() {
+        return Puertas;
+    }
+
+    public void setPuertas(Integer Puertas) {
+        this.Puertas = Puertas;
+    }
+
+    public Integer getTamaño() {
+        return Tamaño;
+    }
+
+    public void setTamaño(Integer Tamaño) {
+        this.Tamaño = Tamaño;
+    }
+      
+}
